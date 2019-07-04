45d99be596312a324d7539e9628042757360f61b
soniee <soniee@DIN78000396.corp.capgemini.com>
2019-07-04 12:16:18 +0530

    myfirstcommit

---
+++ Myfirstproject/src/main/java/com/cg/Firstproject/MainClass.java
@@ -0,0 +1,15 @@
+package com.cg.Firstproject;
+
+public class MainClass {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome! Sales Application!");
+		SalesClass obj=new SalesClass();
+		obj.show();
+	}
+	
+	public static void display()
+	{
+		System.out.println("Hello! Good Morning!");
+	}
+}
---
+++ Myfirstproject/src/main/java/com/cg/Firstproject/SalesClass.java
@@ -0,0 +1,15 @@
+package com.cg.Firstproject;
+
+public class SalesClass
+{
+	private int[] arr= {1,34,56,7};
+	public void show()
+	{
+		for(int num:arr) 
+		{
+			System.out.println(num);
+			
+		}
+	}
+
+}
